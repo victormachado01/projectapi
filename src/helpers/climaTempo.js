c0bf7654aa2152e2904d8007bdb5ba6d1f3f879a
Leandro Gelain <leandrogelain@pop-os.localdomain>
2020-11-03 16:08:56 -0300

    desbugando o liveshare

--- src/helpers/climaTempo.js
+++ src/helpers/climaTempo.js
@@ -1,26 +1,44 @@
-const climaTempoApiUrl = 'http://apiadvisor.climatempo.com.br/api/v1';
+const climaTempoApiUrl = 'https://apiadvisor.climatempo.com.br/api/v1';
 const token = 'bd7915b6e2fb5e65d284c44dfaffcb71';
 const axios = require('axios');
 
-const resultStructure ={
-  'temperatura': undefined,
-  'umidade': undefined,
-  'datahora': undefined,
-  'condicao': undefined,
-  'pressao': undefined,
-  'venVeloc': undefined,
-  'cidade': undefined,
-  'estado': undefined,
-  'pais': undefined,
-  'fonte': undefined
+const resultStructure = {
+  temperatura: undefined,
+  umidade: undefined,
+  datahora: undefined,
+  condicao: undefined,
+  pressao: undefined,
+  venVeloc: undefined,
+  cidade: undefined,
+  estado: undefined,
+  pais: undefined,
+  fonte: undefined
 }
 
-getCityId = (cityName, UF) => {
-  return axios.get(`${climaTempoApiUrl}/locale/city?name=${cityName}&state=${UF}&token=${token}`).then(response => console.log(response))
+const responseStructure = {
+  temperature: undefined,
+  humidity: undefined,
+  date: undefined,
+  wind_direction: undefined,
+  wind_velocity: undefined,
+  condition: undefined,
+  pressure: undefined,
+  icon: undefined,
+  sensation: undefined,
 }
 
-exports.getWeather = (cityId) => {
-  return axios.get(`${climaTempoApiUrl}/weather/${cityId}/current?token=${token}`).then(response => console.log(response))
+const getCityId = async (cityName, UF) => {
+  //...............//
+  return axios.get(encodeURI(`${climaTempoApiUrl}/locale/city?name=${cityName}&state=${UF}&token=${token}`))
+    .then(response => response.data[0].id)
+    .catch(err => console.log(err.message))
 }
 
-getCityId('Marília', 'SP');
+const getWeather = async () => {
+  const cityId = await getCityId('Marília', 'SP')
+  return axios.get(`${climaTempoApiUrl}/weather/locale/${cityId}/current?token=${token}`)
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err))
+}
+
+getWeather()
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const climaTempoHelper = require('./helpers/climaTempo')
+const cors = require('cors')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(cors())
+app.use(express.json())
 
 app.listen(8080);
